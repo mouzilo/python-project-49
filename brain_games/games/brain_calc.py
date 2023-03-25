bc7f9925f17759626f65a34288cbecc7a1b168c9
mouzilo <6473839@gmail.com>
2023-03-25 15:16:54 +0300

    new architecture

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,29 +1,10 @@
 #!/usr/bin/env python3
 
-import prompt
-from random import randint, choice
-from welcome.cli import welcome_user
 
-def main():
-    welcome_message = welcome_user()
-    counter = 0
-    while counter < 3:
-        print('What is the result of the expression?')
-        rand_int1 = randint(1, 10)
-        rand_int2 = randint(1, 10)
-        operator = choice(["+", "-", "*"])
-
-        print(f'Question: {rand_int1} {operator} {rand_int2}')
-        answer = input('Answer: ')
-
-        if str(eval(f'{rand_int1} {operator} {rand_int2}')) == answer:
-            print('Correct!')
-            counter += 1
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{eval(str(rand_int1) + operator + str(rand_int2))}'.\nLet's try again, {welcome_message}!")
-            return
-
-    print(f'Congratulations, {welcome_message}!')
-
-if __name__ == '__main__':
-    main()
+def brain_calc():
+    num1 = randint(1, 10)
+    num2 = randint(1, 10)
+    operation = choice(['+', '-', '*'])
+    question = f'{num1} {operation} {num2}'
+    correct_answer = str(eval(question))
+    return question, correct_answer
--- brain_games/welcome/cli.py
+++ brain_games/welcome/cli.py
@@ -1,4 +1,10 @@
+#!/usr/bin/env python3
+
+
 import prompt
+from random import randint, choice
+from games.brain_calc import brain_calc
+from games.brain_even import brain_even
 
 
 def welcome_user():
@@ -7,5 +13,22 @@ def welcome_user():
     return name
 
 
+def run_game(game):
+    welcome_message = welcome_user()
+    counter = 0
+    while counter < 3:
+        question, correct_answer = game()
+        print('Question:', question)
+        answer = prompt.string('Your answer: ')
+        if answer == correct_answer:
+            print('Correct!')
+            counter += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\nLet's try again, {welcome_message}!")
+            return
+    print(f'Congratulations, {welcome_message}!')
+
+
+
 if __name__ == '__main__':
-    welcome_user()
+    run_game(game)
