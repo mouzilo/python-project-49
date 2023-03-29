358a3c048c12323fce18b08f76904998ad26bb8f
mouzilo <6473839@gmail.com>
2023-03-29 19:56:17 +0300

    add map

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -12,33 +12,23 @@ from brain_games.games.brain_prime import brain_prime
 def engine(game='brain_even'):
     welcome_message = welcome_user()
     counter = 0
-    if game == 'brain_calc':
-        print('What is the result of the expression?')
-    elif game == 'brain_even':
-        print('Answer "yes" if the number is even, otherwise answer "no".')
-    elif game == 'brain_gcd':
-        print('Find the greatest common divisor of given numbers.')
-    elif game == 'brain_progression':
-        print('What number is missing in the progression?')
-    elif game == 'brain_prime':
-        print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    else:
-        print('Unknown game')
-    while counter < 3:
-        if game == 'brain_calc':
-            question, correct_answer = brain_calc()
-        elif game == 'brain_even':
-            question, correct_answer = brain_even()
-        elif game == 'brain_gcd':
-            question, correct_answer = brain_gcd()
-        elif game == 'brain_progression':
-            question, correct_answer = brain_progression()
-        elif game == 'brain_prime':
-            question, correct_answer = brain_prime()
-        else:
-            print('Unknown game')
-            return
 
+    game_map = {
+        'brain_calc': brain_calc,
+        'brain_even': brain_even,
+        'brain_gcd': brain_gcd,
+        'brain_progression': brain_progression,
+        'brain_prime': brain_prime,
+    }
+
+    if game not in game_map:
+        print('Unknown game')
+        return
+
+    print(game_map[game].__doc__)
+
+    while counter < 3:
+        question, correct_answer = game_map[game]()
         print('Question:', question)
         answer = prompt.string('Your answer: ')
 
@@ -50,4 +40,5 @@ def engine(game='brain_even'):
                   f"Correct answer was '{correct_answer}'.\n"
                   f"Let's try again, {welcome_message}!")
             return
+
     print(f'Congratulations, {welcome_message}!')
